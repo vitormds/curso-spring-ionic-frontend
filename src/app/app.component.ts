2a3db886107384e243eb428ae64889edfd51f415
Vitor Souza <vitormendes.souza@hotmail.com>
2019-03-17 13:11:19 -0300

    Tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,6 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-
-
-
 @Component({
   templateUrl: 'app.html'
 })
